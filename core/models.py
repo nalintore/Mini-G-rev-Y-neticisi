ea17902a209be6674a3b14bc2ec86efaa708da31
Nalin Tore <nalintrr@gmail.com>
2026-07-29 17:25:27 +0300

    Faz 2: Model yazım hataları düzeltildi

--- core/models.py
+++ core/models.py
@@ -1,34 +1,33 @@
 from django.db import models
 
 class Project(models.Model):
-    name = models.CharField(max_length = 255)
-    descripton = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add = True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
-class Task(models.Mpdel):
+class Task(models.Model):
     class StatusChoices(models.TextChoices):
-        TODO = 'TODO', 'todo'
+        TODO = 'TODO', 'Todo'
         IN_PROGRESS = 'IN_PROGRESS', 'In Progress'
         DONE = 'DONE', 'Done'
 
-        title = models.CharField(max_length=255)
-        description = models.TextField(blank = True, null = True)
-        status = models.CharField(
-            max_length=20,
-            choices = StatusChoices.choices,
-            default = StatusChoices.TODO
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoices.choices,
+        default=StatusChoices.TODO
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
-        )
-
-        project = models.ForeignKey(
-            Project,
-            on_delete = models.CASCADE,
-            related_name = 'tasks'
-        )
-
-        def __str__(self):
-            return f"{self.title} ({self.project.name})"
+    def __str__(self):
+        return f"{self.title} ({self.project.name})"
